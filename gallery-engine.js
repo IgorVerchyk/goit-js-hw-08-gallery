21cfb1a9cd2a4dabcfc1fa2341b7fe54ac9b6c93
IgorVerchyk <sabyr.ivua@gmail.com>
2020-06-14 18:07:21 +0300

    src files added

---
+++ gallery-engine.js
@@ -0,0 +1,6 @@
+import gallery from "./gallery-items.js";
+const refs = {
+  list: document.querySelector(".js-gallery"),
+  modalButton: document.querySelector(".lightbox__button"),
+  originImg: document.querySelector(".lightbox__image"),
+};
