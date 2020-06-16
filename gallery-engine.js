6af29fd74a8fc794aa95e849ddbe0c54fa563e1d
IgorVerchyk <sabyr.ivua@gmail.com>
2020-06-16 20:49:26 +0300

    gallery rendering added

--- gallery-engine.js
+++ gallery-engine.js
@@ -4,3 +4,15 @@ const refs = {
   modalButton: document.querySelector(".lightbox__button"),
   originImg: document.querySelector(".lightbox__image"),
 };
+renderGallery();
+
+function renderGallery() {
+  const galleryItem = gallery.map((item) => {
+    return `<li class='gallery__item'>
+		<a class="gallery__link">
+		<img class="gallery__image" src=${item.preview} alt="${item.description}" data-id="${item.id}">
+		</a>
+		</li>`;
+  });
+  refs.list.insertAdjacentHTML("afterbegin", galleryItem.join(""));
+}
